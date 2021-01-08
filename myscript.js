02124c60dc650cc3f999ba995721ffc82521fe74
Mohammed Elsamad M1-76 <mohammed.elsamadss@gmail.com>
2021-01-08 15:01:53 +0300

    done

--- myscript.js
+++ myscript.js
@@ -6,9 +6,61 @@ let board = [
 
 
 
-function press(cellNO) {
+let player = `x`
+
+
+
+
+function press(x,y) {
 
     // Do stuff
-    console.log(cellNO)
-    
+    console.log(x,y)
+
+  if (player == `x`)  {
+      player = `0`
+  } else{
+      player = `x`
+  }
+
+
+
+   board[x][y] = player;
+   
+   let choice = document.getElementById(`${x}${y}`);
+   choice.innerHTML = player;
+
+  
+
+   if (player == board[0][0] && player == board[0][1] && player == board[0][2]) {
+       alert(`فزت ${player}`)
+   } else if ( player == board[1][0] && player == board[1][1] && player == board[1][2]) {
+       alert(`فزت ${player}`) 
+   } else if ( player == board[2][0] && player == board[2][1] && player == board[2][2]) {
+       alert(`فزت ${player}`)
+   } else if ( player == board[0][0] && player == board[1][0] && player == board[2][0]) {
+       alert(`فزت ${player}`) 
+   } else if ( player == board[0][1] && player == board[1][1] && player == board[2][1]) { 
+       alert(`فزت ${player}`)
+   } else if ( player == board[0][2] && player == board[1][2] && player == board[2][2]) { 
+       alert(`فزت ${player}`)
+   } else if ( player == board[0][0] && player == board[1][1] && player == board[2][2]) { 
+       alert(`فزت ${player}`)
+   } else if ( player == board[0][2] && player == board[1][1] && player == board[2][0]) { 
+       alert(`فزت ${player}`)
+   }
+
+ 
+  
+
+
 }
+
+
+
+
+
+
+
+
+
+
